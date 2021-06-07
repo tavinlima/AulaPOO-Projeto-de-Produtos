0849165bf16cea0984f765ec2688d8cf937ef943
Gustavo Lima <glima.0913@gmail.com>
2021-06-07 18:29:44 -0300

    Classe de login iniciada

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,7 +1,23 @@
+using ProjetoProdutos.Interfaces;
+
 namespace ProjetoProdutos.Classes
 {
-    public class Login
+    public class Login : ILogin
     {
-        
+        public bool Logado = true;
+        public string Deslogar(Usuario usuario)
+        {
+            return "O usuario deslogou";
+        }
+
+        public string Logar(Usuario usuario)
+        {
+            return "O usuario logou";
+        }
+
+        void ILogin.Login()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Interfaces/ILogin.cs
+++ Interfaces/ILogin.cs
@@ -1,7 +1,11 @@
+using ProjetoProdutos.Classes;
+
 namespace ProjetoProdutos.Interfaces
 {
     public interface ILogin
     {
-         
+        void Login();
+        string Logar(Usuario usuario);
+        string Deslogar(Usuario usuario);
     }
 }
