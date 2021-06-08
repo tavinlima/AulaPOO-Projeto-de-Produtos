15fe2a10d5ae7f73a8f79794ac9329af87573a23
Gustavo Lima <glima.0913@gmail.com>
2021-06-08 15:58:47 -0300

    Adição de cadastro de produtos e de marcas em na classe Login

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,23 +1,91 @@
+using System;
 using ProjetoProdutos.Interfaces;
 
 namespace ProjetoProdutos.Classes
 {
     public class Login : ILogin
     {
-        public bool Logado = true;
+        public bool Logado { get; set; }
+        public bool opcaoValida { get; set; }
         public string Deslogar(Usuario usuario)
         {
+            if (Logado)
+            {
+                Logado = false;
+                Console.WriteLine("Obrigado por utilizar em nosso sistema, até breve!");
+            }
             return "O usuario deslogou";
         }
 
         public string Logar(Usuario usuario)
         {
+            if (!Logado)
+            {
+                Console.WriteLine("Obrigado por utilizar em nosso sistema, para entrar");
+            }
             return "O usuario logou";
         }
 
         void ILogin.Login()
         {
-            throw new System.NotImplementedException();
+            do
+            {
+
+                Console.WriteLine($@"
+O que deseja fazer?
+
+1 - Cadastrar usuário
+2 - Cadastrar produtos
+3 - Cadastrar Marcas
+            ");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Insira o nome de usuário: ");
+                        string NomeUsuario = Console.ReadLine();
+
+                        Console.WriteLine("Insira o e-mail de usuário: ");
+                        string EmailUsuario = Console.ReadLine();
+
+                        Console.WriteLine("Insira a senha de usuário: ");
+                        string SenhaUsuario = Console.ReadLine();
+
+                        Usuario usuario = new Usuario(123456, NomeUsuario, EmailUsuario, SenhaUsuario);
+                        usuario.Cadastrar(usuario);
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Insira o código do produto: ");
+                        int CodigoProduto = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Insira o nome do produto: ");
+                        string NomeProduto = Console.ReadLine();
+
+                        Console.WriteLine("Insira o preço do produto: ");
+                        float PrecoProduto = float.Parse(Console.ReadLine());
+
+                        Produto produto = new Produto();
+
+                        produto.Cadastrar(produto);
+                        produto.Listar();
+                        break;
+                    case "3":
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("OPÇÃO INVÁLIDA!!!");
+                        Console.ResetColor();
+                        opcaoValida = false;
+                        break;
+                }
+                Marca marca = new Marca();
+            } while (!opcaoValida);
+
         }
     }
 }
